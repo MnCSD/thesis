fdee322c3add28b879990aa89da9c682320db7d7
Marios Nikolopoulos <mnikolopoulos@192.168.2.9>
2024-11-21 18:08:57 +0200

    updates

--- convex/auth.ts
+++ convex/auth.ts
@@ -1,9 +1,10 @@
 import { convexAuth } from "@convex-dev/auth/server";
 import GitHub from "@auth/core/providers/github";
-import { Password } from "@convex-dev/auth/providers/password";
+import { Password } from "@convex-dev/auth/providers/Password";
 import { DataModel } from "./_generated/dataModel";
 
 const CustomPassword = Password<DataModel>({
+  // @ts-expect-error "Invalid type" error
   profile(params) {
     return {
       email: params.email as string,
